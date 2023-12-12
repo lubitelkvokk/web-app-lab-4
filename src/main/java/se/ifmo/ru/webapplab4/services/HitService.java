cda45d9c599d4a5c34b93b46020b31b96ea47a47
lubitelkvokk <pasha110.mikhailov@yandex.ru>
2023-12-12 13:41:28 +0300

    fixed bag with hitting calc

--- src/main/java/se/ifmo/ru/webapplab4/services/HitService.java
+++ src/main/java/se/ifmo/ru/webapplab4/services/HitService.java
@@ -37,15 +37,20 @@ public class HitService {
         Double x = hitEntity.getX();
         Double y = hitEntity.getY();
         Double r = hitEntity.getR();
+
+        System.out.println("X VALUE: " + x);
+        System.out.println("Y VALUE: " + y);
+        System.out.println("R VALUE: " + r);
+        System.out.println(x < -3 || x > 5);
         if (x < -3 || x > 5) {
             throw new HitBoundaryException("value X must be between -3 and 5");
         } else if (y < -5 || y > 3) {
             throw new HitBoundaryException("value Y must be between -5 and 3");
         } else if (r < 0 || r > 5) {
-            throw new HitBoundaryException("value R must be between -3 and 5");
+            throw new HitBoundaryException("value R must be between 0 and 5");
         } else {
-            if (x * x + y * y <= r * r && x <= 0 && y > 0
-                    || y < (-x * r / 2) && x >= 0 && y >= 0
+            if (x * x + y * y <= r * r / 4 && x <= 0 && y >= 0
+                    || y <= (-x + r / 2) && x >= 0 && y >= 0
                     || x >= 0 && x <= r / 2 && y <= 0 && y >= -r) {
                 return true;
             }
